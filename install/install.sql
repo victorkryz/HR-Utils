0da03152b358ec4b33976ca5c50351f2c09e5eee
victorkryz <victor.kryz@gmail.com>
2017-03-24 19:31:46 +0200

    version check

--- install/install.sql
+++ install/install.sql
@@ -14,22 +14,20 @@ declare
  E_UNSUITABLE_SERVER_VERSION constant int := -20100;
  
      function to_ver_str(p_ver in pls_integer, p_release in pls_integer) 
-                                            return varchar2 as
-     v_str varchar2(20) := p_ver || '.' || p_release;                                       
+                                                       eturn varchar2 as
+      v_str varchar2(20) := p_ver || '.' || p_release;                                       
      begin
-     return v_str;
+      return v_str;
      end to_ver_str;
 begin
     if ( (DBMS_DB_VERSION.VERSION < SUITABLE_MIN_VERSION) or 
          ((DBMS_DB_VERSION.VERSION = SUITABLE_MIN_VERSION) and (DBMS_DB_VERSION.RELEASE < SUITABLE_MIN_RELEASE)) ) then
        raise_application_error(E_UNSUITABLE_SERVER_VERSION, 
                                'Oracle server version (' || to_ver_str(DBMS_DB_VERSION.VERSION, DBMS_DB_VERSION.VERSION) || 
-                               ') lower than minimal suitable (' || 
-                               to_ver_str(SUITABLE_MIN_VERSION, SUITABLE_MIN_RELEASE) || ')');
+                               ') lower than minimal suitable (' || to_ver_str(SUITABLE_MIN_VERSION, SUITABLE_MIN_RELEASE) || ')');
     end if;
 end;
 /
-
 
 prompt  ... - synonyms ...
 
